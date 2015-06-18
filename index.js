75a919650b4ba72856156793875f606e4c650661
David Von Lehman <david.vonlehman@gmail.com>
2015-06-17 21:15:30 -0700

    Unit tests for serve function

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var _ = require('lodash');
 var debug = require('debug')('4front:deployer');
 
-require('simple-errors');
-
 module.exports = function(settings) {
   if (!settings.database)
     throw new Error("Missing database option");
@@ -20,32 +18,10 @@ module.exports = function(settings) {
   exports.bundle = require('./lib/bundle')(settings);
 
   // Create,update, and delete versions
-  exports.version = require('./lib/version')(settings);
+  exports.versions = require('./lib/version')(settings);
 
   // Serve deployed static assets
   exports.serve = require('./lib/serve')(settings);
 
-  // return {
-  //   createVersion: createVersion,
-  //   updateVersionStatus: updateVersionStatus,
-  //   deployFile: deployFile,
-  //   deleteVersion: deleteVersion,
-  //   deleteAllVersions: deleteAllVersions,
-  //   deployArchive: deployArchive,
-  //   serveFile: serveFile
-  // };
-
-  // Split the collection into equal size groups
-  // function splitIntoGroups(coll, size) {
-  //   if (coll.length < size)
-  //     return [coll];
-  //
-  //   var numGroups = Math.ceiling(coll.length / size);
-  //
-  //   var groups = [];
-  //   for (var i=0; i<numGroups; i++) {
-  //     groups.push(_.slice(coll, i * size, size));
-  //   }
-  //   return groups;
-  // }
+  return exports;
 };
---
+++ test/serve.js
@@ -0,0 +1,108 @@
+var assert = require('assert');
+var _ = require('lodash');
+var sinon = require('sinon');
+var shortid = require('shortid');
+var zlib = require('zlib');
+var sbuff = require('simple-bufferstream');
+var through = require('through2');
+
+require('dash-assert');
+
+describe('version', function() {
+  var self;
+
+  beforeEach(function() {
+    self = this;
+    this.settings = {};
+
+    this.settings = {
+      storage: {
+        readFileStream: sinon.spy(function(storagePath) {
+          return sbuff(self.contents);
+        }),
+        getMetadata: sinon.spy(function(storagePath, callback) {
+          callback(null, self.metadata);
+        })
+      },
+      defaultMaxAge: 1000
+    };
+
+    this.metadata = {};
+    this.serve = require('../lib/serve')(this.settings);
+
+    this.appId = shortid.generate();
+    this.versionId = shortid.generate();
+
+    this.output = '';
+
+    this.res = through(function(chunk, enc, cb) {
+      self.output += chunk.toString();
+      this.push(chunk);
+      cb();
+    });
+
+    this.res.set = sinon.spy(function(header, value) {});
+  });
+
+  it('serves file', function(done) {
+    var filePath = 'pages/home.html';
+
+    this.metadata = {
+      ContentType: 'text/html'
+    };
+
+    this.contents = "<html></html>";
+
+    this.res.on('finish', function() {
+      assert.equal(self.output, self.contents);
+
+      assert.isTrue(self.settings.storage.getMetadata.calledWith(
+        self.appId + '/' + self.versionId + '/pages/home.html'));
+
+      assert.isTrue(self.res.set.calledWith(
+        'Cache-Control', 'maxage=' + self.settings.defaultMaxAge));
+
+      done();
+    });
+
+    this.serve(this.appId, this.versionId, filePath, this.res);
+  });
+
+  it('serve gzipped file', function(done) {
+    var filePath = 'js/app.js';
+
+    this.metadata = {
+      ContentType: 'application/javascript',
+      ContentEncoding: 'gzip'
+    };
+
+    this.res.on('finish', function() {
+      assert.isTrue(self.res.set.calledWith('Content-Encoding', 'gzip'));
+
+      done();
+    });
+
+    this.contents = "made_up_gzip_content";
+    this.serve(this.appId, this.versionId, filePath, this.res);
+  });
+
+  it('serves with custom Cache-Control', function(done) {
+    var filePath = 'pages/home.html';
+
+    this.metadata = {
+      ContentType: 'text/html',
+      CacheControl: 'private'
+    };
+
+    this.contents = "<html></html>";
+
+    this.res.on('finish', function() {
+      assert.isTrue(self.res.set.calledWith(
+        'Cache-Control', 'private'));
+
+      done();
+    });
+
+    this.serve(this.appId, this.versionId, filePath, this.res);
+  });
+});
