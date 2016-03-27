84fb3cdd3cfae4af0577990c33297f860a7d7448
David Von Lehman <david.vonlehman@gmail.com>
2016-03-26 21:04:59 -0700

    Show npm progress

--- engines/common.js
+++ engines/common.js
@@ -91,7 +91,7 @@ module.exports.runNpmInstall = function(params, moduleName, callback) {
 
   var npmArgs = ['install'];
   if (moduleName) npmArgs.push(moduleName);
-  npmArgs.push('--progress', 'false');
+  // npmArgs.push('--progress', 'false');
 
   var spawnArgs = {
     executable: params.npmExecutable,
